85ed1a1b4450e96876e813dbe5e1fb4e578ffd46
David De Sousa <david.desousa@diffagency.com>
2020-02-22 23:49:01 -0500

    Storybook compiled as static site to /docs. Added Story addon for code tab.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,7 @@
 module.exports = {
   stories: ['../src/**/*.stories.[tj]s'],
-  addons: ['@storybook/addon-a11y/register']
+  addons: [
+    '@storybook/addon-a11y/register',
+    '@storybook/addon-storysource',
+  ]
 };
---
+++ docs/main.b03267b81ab29b5f7204.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],[function(n,o,p){p(413),p(555),n.exports=p(1228)},function(n,o){}],[[0,1,2]]]);
